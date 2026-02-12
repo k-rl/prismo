7e6a49f0221cfc0ca92b3b31f218948db9a3adca
Karl Krauth <karl.krauth@gmail.com>
2026-02-11 23:03:11 -0800

    Remove fluigent.

---
+++ src/prismo/devices/fluidic/__init__.py
@@ -0,0 +1,3 @@
+from .fluidic import FlowController
+
+__all__ = ["FlowController"]
--- src/prismo/devices/fluigent/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-
-if os.name == "nt":
-    # Enable local dll loading for editable pip installs.
-    os.add_dll_directory(os.path.dirname(os.path.realpath(__file__)))
-    from .fluigent import FlowController
